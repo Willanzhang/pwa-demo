caca18164f1546a562adc4d6a87f380ae2e1337c
unknown <WillianZhang@snsshop.net>
2018-01-23 15:33:07 +0800

    ok

---
+++ sw.js
@@ -0,0 +1,45 @@
+var cacheStorageKey = 'minimal-pwa-1'
+
+// 处理静态缓存
+var cacheList = [
+  '/',
+  "index.html",
+  "main.css",
+  "e.png"
+]
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheStorageKey)
+    .then(cache => cache.addAll(cacheList))
+    .then(() => self.skipWaiting())
+  )
+})
+
+// 处理动态缓存
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      if (response != null) {
+        return response
+      }
+      return fetch(e.request.url)
+    })
+  )
+})
+
+//更新静态资源
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    Promise.all(
+      caches.keys().then(cacheNames => {
+        return cacheNames.map(name => {
+          if (name !== cacheStorageKey) {
+            return caches.delete(name)
+          }
+        })
+      })
+    ).then(() => {
+      return self.clients.claim()
+    })
+  )
+})
